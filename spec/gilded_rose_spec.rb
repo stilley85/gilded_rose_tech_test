a06f573e65a1906ad532c3d9a3b037a5d71ef396
Stephen Tilley <stephentilley@gmail.com>
2018-03-27 12:22:53 +0100

    Tests added for standard items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,11 +2,41 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
+  describe '#update_quality' do
+    it 'does not change the name' do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
+    end
+  end
+
+  context '#standard_item' do
+
+    before(:each) do
+      @items = [Item.new("foo", 5, 10), Item.new("foo", 5, 1), Item.new("foo", 1, 10)]
+      GildedRose.new(@items).update_quality()
+    end
+
+    it 'sell_in decreases by 1 each day' do
+      expect(@items[0].sell_in).to eq(4)
+    end
+
+    it 'quality decreases by 1 each day' do
+      expect(@items[0].quality).to eq(9)
+    end
+
+    it 'Quality cannot fall below 0' do
+      5.times do
+        GildedRose.new(@items).update_quality()
+      end
+      expect(@items[1].quality).to eq(0)
+    end
+
+    it 'Quality reduces by 2 each day if sell_in is below 0' do
+      2.times do
+        GildedRose.new(@items).update_quality()
+      end
+      expect(@items[2].quality).to eq(5)
     end
   end
 
