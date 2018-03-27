ff0d716acb9496311d1932d3bf80aba5d441afc0
Stephen Tilley <stephentilley@gmail.com>
2018-03-27 20:27:40 +0100

    Extracted logic for deadline items into its own method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,19 +11,7 @@ class GildedRose
       elsif item.name == "Aged Brie"
         maturing_item(item)
       elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.sell_in -= 1
-        if item.sell_in < 0
-          item.quality = 0
-        elsif item.sell_in < 5
-          item.quality += 3
-          over_max?(item)
-        elsif item.sell_in < 10
-          item.quality += 2
-          over_max?(item)
-        else
-          item.quality += 1
-          over_max?(item)
-        end
+        deadline_item(item)
       else
         normal_item(item)
       end
@@ -39,6 +27,12 @@ class GildedRose
   def maturing_item(item)
     item.sell_in -= 1
     item.sell_in > 0 ? item.quality += 1 : item.quality += 2
+    over_max?(item)
+  end
+
+  def deadline_item(item)
+    item.sell_in -= 1
+    item.sell_in < 0 ? item.quality = 0 : item.sell_in < 5 ? item.quality += 3 : item.sell_in < 10 ? item.quality += 2 : item.quality += 1
     over_max?(item)
   end
 
